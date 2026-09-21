e4ec9a110059492bbc81a2eef8611bab97c20dbc
vairavelu1989-vel <vairavelu1989@gmail.com>
2026-09-21 10:41:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require('fs'); // ஃபைல்களைப் படிக்கவும் எழுதவும் உதவும் டூல்
+const { request } = require('http');
+const { writer } = require('repl');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -36,6 +38,26 @@ app.post('/api/products', (request, response) => {
     writeData(gadgets); // அதை மீண்டும் data.json ஃபைலில் நிரந்தரமாக எழுதுகிறோம் (Save)
     
     response.status(201).json(newProduct);
+});
+
+// [DELETE] 5. குறிப்பிட்ட ஒரு பொருளை ID மூலம் நீக்கும் முகவரி
+app.delete('/api/products/:id', (request, response) => {
+    const productId = parseInt(request.params.id);
+    let gadgets = readData(); // ஃபைலில் இருந்து தரவுகளைப் படிக்கிறோம்
+
+    // நாம் அனுப்பிய ID பட்டியலில் இருக்கிறதா என்று சரிபார்க்கிறோம்
+    const productExists = gadgets.some(item => item.id === productId);
+
+    if (productExists) {
+        // 'filter' முறையைப் பயன்படுத்தி அந்த ID-ஐத் தவிர மற்ற அனைத்துப் பொருட்களையும் புதிய லிஸ்ட்டாக மாற்றுகிறோம்
+        gadgets = gadgets.filter(item => item.id !== productId);
+        
+        writeData(gadgets); // புதிய லிஸ்ட்டை மீண்டும் data.json ஃபைலில் சேமிக்கிறோம் (Save)
+        
+        response.json({ message: `பொருள் (ID: ${productId}) வெற்றிகரமாக நீக்கப்பட்டது!` });
+    } else {
+        response.status(404).json({ message: "மன்னிக்கவும்! அந்தப் பொருள் பட்டியலில் இல்லை." });
+    }
 });
 
 app.listen(PORT, () => {
